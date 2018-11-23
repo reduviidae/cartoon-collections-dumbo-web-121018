53852a4d9495ee835a954898c2c2e35a51a3e3b9
Wil Cook <wil.cook.is@gmail.com>
2018-11-23 19:29:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |call|
+  planeteer_calls.each do |call|
     puts call.capitalize << "!"
   end
 end
