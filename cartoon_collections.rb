c362af7798cb07275143ee0c3a18c0f66fcf68ac
Wil Cook <wil.cook.is@gmail.com>
2018-11-23 19:11:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, number|
+    puts "#{number + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |call|
+    puts call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.collect do |call|
+    if call.length > 4
+      return true
+    else 
+      return false
+  end
 end
 
 def find_the_cheese# code an argument here
