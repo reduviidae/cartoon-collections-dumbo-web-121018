77629af86e7139a296ef96175933d69a0d19c1c8
Wil Cook <wil.cook.is@gmail.com>
2018-11-23 19:24:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,12 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.collect do |call|
-    if call.length > 4
-      return true
-    else 
-      return false
-  end
+  calls.any? 
 end
 
 def find_the_cheese# code an argument here
